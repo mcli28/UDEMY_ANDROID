b7a4e0c28c28ec9f42ca539ca2e2b9c1e79b780b
mcli28 <mclimber28@gmail.com>
2024-07-28 10:20:56 -0400

    terminado el menu y menu searchview

--- TheCompleteAndroidAppDevelopment/MyAndroidLayouts/app/src/main/java/com/hobbydev/myandroidlayouts/MainActivity.kt
+++ TheCompleteAndroidAppDevelopment/MyAndroidLayouts/app/src/main/java/com/hobbydev/myandroidlayouts/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.hobbydev.myandroidlayouts
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MenuInflater
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.mainmenu, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.search_bar)
+        val searchView: SearchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Acciones cuando se envía la búsqueda
+                // Puedes agregar aquí tu lógica de búsqueda
+                Toast.makeText(applicationContext, query, Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Acciones cuando el texto cambia
+                // Puedes agregar aquí tu lógica para filtrar resultados en tiempo real
+                return false
+            }
+        })
+
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection.
+        return when (item.itemId) {
+            R.id.Home -> {
+                Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.Add -> {
+                Toast.makeText(this, "Add selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.Setting -> {
+                Toast.makeText(this, "Setting selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
